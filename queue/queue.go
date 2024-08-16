59072b8e8a3cf7374999849d2a98d7c557f5c40e
Yasar Cakir <dev.yasarcakir@gmail.com>
2024-08-17 00:59:17 +0700

    Add Enqueue and Dequeue methods to Queue

--- queue/queue.go
+++ queue/queue.go
@@ -2,6 +2,22 @@ package queue
 
 type Queue []string
 
-func (q *Queue) IsEmpty() {
+func (q *Queue) IsEmpty() bool {
 	return len(*q) == 0
+}
+
+func (q *Queue) Enqueue(str string) {
+	*q = append(*q, str)
+}
+
+func (q *Queue) Dequeue() (string, bool) {
+	if q.IsEmpty() {
+		return "", false
+	}
+
+	index := 0
+	firstElement := (*q)[index]
+	*q = (*q)[index+1:]
+
+	return firstElement, true
 }
