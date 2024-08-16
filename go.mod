3fa47c585a404fd5a1209111a958f614e74a88c7
Yasar Cakir <dev.yasarcakir@gmail.com>
2024-08-17 00:50:06 +0700

    Add .gitignore, README, go.mod, and stack implementation

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ysrckr/go-data-structures
+
+go 1.22.5
---
+++ stack.go
@@ -0,0 +1,23 @@
+package stack
+
+type Stack []string
+
+func (s *Stack) IsEmpty() bool {
+	return len(*s) == 0
+}
+
+func (s *Stack) Push(str string) {
+	*s = append(*s, str)
+}
+
+func (s *Stack) Pop() (string, bool) {
+	if s.IsEmpty() {
+		return "", false
+	}
+
+	index := len(*s) - 1
+	lastElement := (*s)[index]
+	*s = (*s)[:index]
+
+	return lastElement, true
+}
